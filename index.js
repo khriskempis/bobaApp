d70a3f123ea911fe31e6b9cec236f3527699aafa
Khris Kempis <khriskempis@gmail.com>
2018-07-04 22:42:21 -0700

    cleaned index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function getCityFromAPI(searchItem, callback ){
 	$.getJSON(GOOGLE_LOCATION_API_ENDPOINT, query, callback)
 }
 
-function initMap(lat, long){
+function initMap(lat, lng){
 	let map = new google.maps.Map(document.getElementById('display-map'), {
 		center: {
 			lat: parseFloat(lat),
-			lng: parseFloat(long)
+			lng: parseFloat(lng)
 			// lat: -34.397,
 			// lng: 150.644
 		},
@@ -23,22 +23,29 @@ function initMap(lat, long){
 	})
 }
 
+function findSurroundingVenues(lat, lng){
+	let query ={
+		
+	}
+};
+
 function generateMap(data){
 
 	let city = data.results[0].formatted_address
 	let lat = data.results[0].geometry.location.lat;
-	let long = data.results[0].geometry.location.lng;
+	let lng = data.results[0].geometry.location.lng;
 
 	const htmlString = `<hr>
 			<div role="contentinfo" class="search-item">
 				<h3>${city}</h2>
 				<p>${lat}</p>
-				<p>${long}</p>
+				<p>${lng}</p>
 			</div>
 			<hr>`
 
-	initMap(lat, long)
+	initMap(lat, lng)
 
+	findSurroundingVenues(lat, lng)
 
 
 	renderSearchResults(htmlString);
